7a80d5c2c2c476b31939488ed96b511ad8c6cbe0
itsnjagz <njagzdev@gmail.com>
2024-02-09 15:34:24 +0300

    final passenger counter app

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-let myAge = 27;
-let humanDogRatio = 7;
 
-let myDogAge = myAge * humanDogRatio;
-console.log(myDogAge);
+// THE ONCLICK EVENT LISTENER
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById('save-el')
+
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+
+function save() {
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let saveVar = count + " - ";
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.textContent += saveVar;
+    // NB: Make sure to not delete the existing content of the paragraph
+    
+    //To ensure count resets after saving
+    countEl.textContent = 0;
+    count = 0;
+
+}
+
---
+++ markup.js
@@ -0,0 +1,49 @@
+// let myAge = 27;
+// let humanDogRatio = 7;
+
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+// let bonusPoints = 50;
+// console.log(bonusPoints);
+// bonusPoints += 50;
+// console.log(bonusPoints);
+// bonusPoints -= 75;
+// console.log(bonusPoints);
+// bonusPoints += 45;
+// console.log(bonusPoints);
+
+
+// // Create a function that logs out the sum of all the lap times
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// // Create a function that logs out the sum of all the lap times
+// function logLapTime() {
+//     let totalTime = lap1 + lap2 + lap3
+//     console.log(totalTime)
+// }
+
+// let lapsCompleted = 0
+
+// // Create a function that increments the lapsCompleted variable with one
+// // Run it three times
+// function increaseLaps(){
+//     lapsCompleted+=1;
+// }
+// increaseLaps();
+// increaseLaps();
+// increaseLaps();
+
+// console.log(lapsCompleted)
+
+
+
+
+// let username = "per"
+// // // Create a variable, message, that stores the string: "You have tree new notifications"
+// // let message = "You have three new notifications";
+// // messageToUser= message + ", " + username;
+// //  console.log(messageToUser);
+
